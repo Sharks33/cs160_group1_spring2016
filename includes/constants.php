bf09aff42de6645d19ba1a4ed77fcf9d1aa6fc2a
Will <WillWcchan@gmail.com>
2016-04-13 16:08:42 -0700

    Fixed the db connection string.

--- includes/constants.php
+++ includes/constants.php
@@ -1,10 +1,17 @@
 <?php
 
-define('DB_SERVER', 'www.lunarelec.com');
-define('DB_USER', 'wayknack');
-define('DB_PASSWORD', 'CS174_SJSU_WAYKNACK');
-define('DB_NAME', 'wayknack');
-define('DSN', 'mysql:host=www.lunarelec.com;dbname=wayknack');
+// define('DB_SERVER', 'www.lunarelec.com');
+// define('DB_USER', 'wayknack');
+// define('DB_PASSWORD', 'CS174_SJSU_WAYKNACK');
+// define('DB_NAME', 'wayknack');
+// define('DSN', 'mysql:host=www.lunarelec.com;dbname=wayknack');
+// define('DB_PORT', '3306');
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'cs160grocery');
+define('DB_PASSWORD', 'allenbui');
+define('DB_NAME', 'my_cs160grocery');
+define('DSN', 'mysql:host=localhost;dbname=my_cs160grocery');
 define('DB_PORT', '3306');
 
 ?>
